bddc276a0f17898ba240555bf257ebc1afe5b227
Rajesh Ranjan <rajeshranjan8271@gmail.com>
2024-08-25 08:24:21 +0530

    deleting everything

--- static/js/containers/portfolio/Lightbox/data/slides.jsx
+++
@@ -1,8 +0,0 @@
-import { projectsHeader } from '../../../../portfolio';
-
-export const advancedSlides = projectsHeader.projects.map((project,key)=>({
-  src:project.image,
-  title:project.title,
-  description:project.subtitle,
-  filter: project.filter
-}))
